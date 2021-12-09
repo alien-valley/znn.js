49943b9f9874393673135e7d35916d5a138c039c
VONSBÄK <alien-valley-vonsbak@protonmail.com>
2021-12-09 09:55:08 +0100

    sanity dart code & argon2 since it can't be found

---
+++ sanity/test/sanity_test.dart
@@ -0,0 +1,15 @@
+
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(true, isTrue);
+    });
+  });
+}
