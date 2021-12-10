11a6427281f61e2c4aa36a0871411ac686313440
VONSBÄK <alien-valley-vonsbak@protonmail.com>
2021-12-10 17:18:10 +0100

    convert pub key to bech32 address & parse from string

--- src/importFromMnemonic.js
+++ src/importFromMnemonic.js
@@ -1,5 +1,7 @@
 const bip39 = require('bip39');
 const {getPublicKey, derivePath} = require('ed25519-hd-key');
+const {SHA3} = require('sha3');
+const {bech32} = require('bech32');
 
 // target is to obtain the baseAddress from the keyFile from the mnemonic
 async function main() {
@@ -29,6 +31,38 @@ async function main() {
 
     // should get 881967d6529347a07f73ee2c5f0596b1b4bce44b828ac0a1fd77a0c3f1903559
     console.log(pub);
+
+    // address from pub-key, from the dart SDK
+    /*
+        Address(String hrp, List<int> core) {
+            this.hrp = hrp;
+            this.core = core;
+        }
+
+        static Address fromPublicKey(List<int> publicKey) {
+            var sha3 = SHA3(NORMAL_BITS[1], SHA3_PADDING, SHA3_PADDING[1]);
+            sha3.update(publicKey);
+            var digest = sha3.digest().sublist(0, 19);
+
+            return Address('z', [userByte, ...digest]);
+        }
+     */
+
+    const core = Buffer.concat([Uint8Array.from([0]), SHA3(256).update(pub).digest().subarray(0, 19)]);
+
+    // should get 000b3f243d034ff0e3f2ecda2d9aa65db857552b
+    console.log(core)
+    const addressStr = (bech32.encode("z", bech32.toWords(core)));
+
+    // should get z1qq9n7fpaqd8lpcljandzmx4xtku9w4ftwyg0mq
+    console.log(addressStr);
+
+    // parse address
+    const { prefix, words } = bech32.decode("z1qq9n7fpaqd8lpcljandzmx4xtku9w4ftwyg0mq");
+    let extractedCore = new Buffer.from(bech32.fromWords(words));
+
+    // should get 'z' 000b3f243d034ff0e3f2ecda2d9aa65db857552b
+    console.log(prefix, extractedCore)
 }
 
 main()
