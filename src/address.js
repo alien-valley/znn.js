e20b21ee84b47f6a95e23105733fdd9f64910fa4
VONSBÄK <alien-valley-vonsbak@protonmail.com>
2021-12-14 13:56:24 +0100

    add empty address

--- src/address.js
+++ src/address.js
@@ -10,7 +10,7 @@ class Address {
     }
 
     static Parse(str) {
-        const { prefix, words } = bech32.decode(str);
+        const {prefix, words} = bech32.decode(str);
         let extractedCore = new Buffer.from(bech32.fromWords(words));
         if (prefix !== 'z') {
             throw `invalid prefix ${prefix} should be 'z'`
@@ -19,4 +19,9 @@ class Address {
     }
 }
 
-module.exports = Address;
+const EmptyAddress = Address.Parse("z1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqsggv2f");
+
+module.exports = {
+    Address,
+    EmptyAddress
+};
