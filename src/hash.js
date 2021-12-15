75b3f12dc261a4a6a89d1db0e27c97e9a6fedb82
VONSBÄK <alien-valley-vonsbak@protonmail.com>
2021-12-15 15:55:25 +0100

    Add hash class

---
+++ src/hash.js
@@ -0,0 +1,21 @@
+const {SHA3} = require("sha3");
+
+class Hash {
+    constructor(core) {
+        this.core = core;
+    }
+
+    toString() {
+        return this.core.toString("hex");
+    }
+
+    static Parse(hexString) {
+        return new Hash(Buffer.from(hexString, "hex"));
+    }
+
+    static Digest(data) {
+        return new Hash(SHA3(256).update(data).digest());
+    }
+}
+
+module.exports = Hash;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const {Address, EmptyAddress} = require("./address")
 const KeyFile = require("./keyFile")
 const KeyPair = require("./keyPair")
+const Hash = require("./hash")
 
 module.exports = {
     Address,
     EmptyAddress,
     KeyFile,
     KeyPair,
+    Hash,
 }
