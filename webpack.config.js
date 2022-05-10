cdd030c4083d047585859460b49aaf9671533722
VONSBÄK <alien-valley-vonsbak@protonmail.com>
2022-05-10 11:15:29 +0200

    fix: use production for webpack to avoid use of eval function

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: {
         main: './src/index.js'
     },
